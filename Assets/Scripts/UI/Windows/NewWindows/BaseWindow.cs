ec854b5f896366f6321a5922afc43bb30612e5fa
Zkov <zkov@yandex.ru>
2018-07-03 01:47:29 +0300

    Add WindowManager

---
+++ Assets/Scripts/UI/Windows/NewWindows/BaseWindow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace UI.Windows
+{
+    public abstract class BaseWindow<T>:MonoBehaviour where T:BaseWindowArgs
+    {
+        public void Show(T windowArgs = null)
+        {
+            OnShow(windowArgs);
+        }
+
+        public void SetInteractive(bool interactive)
+        {
+            OnSetInteractive(interactive);
+        }
+
+        public void SetActive(bool active)
+        {
+            OnSetActive(active);
+        }
+        
+        public void Hide()
+        {
+            OnHide();
+        }
+
+        protected abstract void OnShow(T windowArgs = null);
+        protected abstract void OnSetInteractive(bool interactive);
+        protected abstract void OnSetActive(bool active);
+        protected abstract void OnHide();
+    }
+}
---
+++ Assets/Scripts/UI/Windows/NewWindows/BaseWindowArgs.cs
@@ -0,0 +1,7 @@
+﻿namespace UI.Windows
+{
+    public class BaseWindowArgs
+    {
+        
+    }
+}
